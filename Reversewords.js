2e53b92df9c1a9851a22ee8addbabcf028b8e606
PunnamRadhika3 <143921458+PunnamRadhika3@users.noreply.github.com>
2023-10-09 15:25:33 +0530

    Add files via upload

---
+++ Reversewords.js
@@ -0,0 +1,28 @@
+function reverseWords(sentence) {
+   
+    const words = sentence.split(' ');
+
+   
+    const reversedWords = words.map(word => {
+        return word.split('').reverse().join('');
+    });
+
+    
+    const reversedSentence = reversedWords.join(' ');
+
+    return reversedSentence;
+}
+
+ 
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a sentence: ', (inputSentence) => {
+    const reversedSentence = reverseWords(inputSentence);
+    console.log('Reversed Sentence: ' + reversedSentence);
+    rl.close();
+    s.close();
+});
